bcd9ed24a7fc3419752d2e4419aed65f9ad6ee8f
zh_liw <2011371646@qq.com>
2020-09-04 17:49:44 +0800

    1

---
+++ center/src/test/java/com/zj/freelypay/center/center/CenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zj.freelypay.center.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
